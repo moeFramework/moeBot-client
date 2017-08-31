213d64546249da37cdb1511e6e775764b4143192
Ayaka Neko <neko@ayaka.moe>
2017-08-31 17:37:26 -0600

    Initialized the Plugin Project

---
+++ moePlugin/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using moeInterface;
+
+namespace moePlugin
+{
+    public class Plugin : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "First Plugin";
+            }
+        }
+
+        public string Do()
+        {
+            return "Do Something in First Plugin";
+        }
+    }
+}
